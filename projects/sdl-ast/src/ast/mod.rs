97ad616b40259578c1891c6e531fcb287e82f53a
Aster <galaster@foxmail.com>
2020-07-29 00:21:38 +0800

    🌿 Define ast structure

---
+++ projects/sdl-ast/src/ast/mod.rs
@@ -0,0 +1,65 @@
+use std::{
+    collections::HashMap,
+    fmt::{self, Display, Formatter},
+};
+use crate::TextRange;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum AST {
+    Node {
+        kind: ASTKind,
+        //      children: Box<[AST]>,
+        children: Vec<AST>,
+        r: Option<Box<TextRange>>,
+    },
+    Leaf {
+        kind: ASTKind,
+        r: Option<Box<TextRange>>,
+    },
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum ASTKind {
+    None,
+    Statements,
+
+}
+
+impl Default for AST {
+    fn default() -> Self {
+        Self::Leaf { kind: ASTKind::None, r: Default::default() }
+    }
+}
+
+impl AST {
+    pub fn kind(&self) -> ASTKind {
+        match self {
+            Self::Node { kind, .. } | Self::Leaf { kind, .. } => kind.to_owned(),
+        }
+    }
+
+    pub fn children(&self) -> Vec<AST> {
+        match self {
+            Self::Node { children, .. } => children.to_vec(),
+            Self::Leaf { .. } => vec![],
+        }
+    }
+    pub fn range(&self) -> TextRange {
+        match self {
+            Self::Node { r, .. } | Self::Leaf { r, .. } => r.clone().unwrap_or_default().as_ref().clone(),
+        }
+    }
+}
+
+impl AST {
+    pub fn statements(children: Vec<AST>, r: TextRange) -> Self {
+        Self::Node { kind: ASTKind::Statements, children, r: box_range(r) }
+    }
+}
+
+fn box_range(r: TextRange) -> Option<Box<TextRange>> {
+    match r.sum() {
+        0 => None,
+        _ => box_range(r),
+    }
+}
---
+++ projects/sdl-ast/src/lib.rs
@@ -0,0 +1,5 @@
+mod ast;
+mod utils;
+
+pub use utils::TextRange;
+pub use ast::{AST,ASTKind};
